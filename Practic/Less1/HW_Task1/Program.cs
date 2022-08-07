73468a88b9c2053694940bb7a0b29bb4927912d9
Alexei Efremov <td-interface@yandex.ru>
2022-08-07 09:50:02 +0400

    TW_Task1 find max from 2

--- Practic/Less1/HW_Task1/Program.cs
+++ Practic/Less1/HW_Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+ 
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Write("Введите число A: ");
+int numA = Convert.ToInt32(ReadLine());
+ 
+Write("Введите число B: ");
+int numB = Convert.ToInt32(ReadLine());
+ 
+if (numB*numB < numA){
+    System.Console.WriteLine("Число А больше B");
+}else{
+    System.Console.WriteLine("Число B больше А");
+}
