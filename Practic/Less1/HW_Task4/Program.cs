6f8996ee59eacc96f5e2522107b17acb24f1bb8f
Alexei Efremov <td-interface@yandex.ru>
2022-08-07 13:00:39 +0400

    HW_Task4 %2 from 1 to N

---
+++ Practic/Less1/HW_Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число N: ");
+int numA = Convert.ToInt32(ReadLine());
+int i = 1;
+
+while (i <= numA)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.WriteLine(i);
+    }
+    i++;
+}
+
+
+
+
+
