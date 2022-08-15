300ac34e26628d4859d88f05e420a6f6867af151
Alexei Efremov <td-interface@yandex.ru>
2022-08-15 20:28:02 +0400

    Task10 Show second digit of 3digits number!

---
+++ Practic/Less2/HW2_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1  не использовать строки, только математически
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число Трехзначное число: ");
+int numA = Convert.ToInt32(ReadLine());
+
+int GetSecondDigitOfNumber(int numA)
+{
+int secondDigitDoble = numA% 100;
+int thirdDigit = secondDigitDoble% 10;
+int secondDigit = (secondDigitDoble - thirdDigit)/10;
+return secondDigit;
+}
+
+int secondDigit = GetSecondDigitOfNumber(numA);
+Console.WriteLine($"Вторая цифра числа  -  {numA} является {secondDigit}");
+
+
