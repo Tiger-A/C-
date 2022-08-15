a3e6b2cb8244ffd1f9ef767ca80d3ff938b395c6
Alexei Efremov <td-interface@yandex.ru>
2022-08-15 19:33:54 +0400

    Task9 вывод случайного числа от 10-99 и показывает наибольшуюю цифру числа  с циклом на 10

--- Practic/Less2/Task9/Program.cs
+++ Practic/Less2/Task9/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вывод случайного числа от 10-99 и показывает наибольшуюю цифру числа
+
+int GetRandomNumberInRange(int minNumber,int maxNumber)
+{
+    int result = new Random().Next(minNumber,maxNumber+1);
+    return result;
+}
+int GetMaxDigitFromNumber(int number)
+{
+    int result = number / 10;
+    int secondDigit = number % 10;
+    if (result < secondDigit)
+    {
+        result = secondDigit;
+    }
+        
+    return result;
+}
+
+for(int i = 0; i < 10; i++)
+{
+int randomNumber = GetRandomNumberInRange(10,99);
+int MaxDigit = GetMaxDigitFromNumber(randomNumber);
+Console.WriteLine($"Наибольшая цифра числа -  {randomNumber} является {MaxDigit}");
+}
