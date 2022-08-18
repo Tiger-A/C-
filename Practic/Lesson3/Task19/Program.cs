8b148fa0b481c073c238d44ad04407a94af83d2d
Alexei Efremov <td-interface@yandex.ru>
2022-08-18 14:50:16 +0400

    Task19 ринимает на вход пятизначное число и проверяет, является ли оно палиндромом

---
+++ Practic/Lesson3/Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+using static System.Console;
+Clear();
+Write("Введите какое-нибудь пятизначное число: ");
+int number = Convert.ToInt32(ReadLine());
+int FirstDigit = number % Convert.ToInt32(Math.Pow(10, 5)) / Convert.ToInt32(Math.Pow(10, 4));
+int SecondDigit = number % Convert.ToInt32(Math.Pow(10, 4)) / Convert.ToInt32(Math.Pow(10, 3));
+int PenultimateDigit = number % Convert.ToInt32(Math.Pow(10, 2)) / Convert.ToInt32(Math.Pow(10, 1));
+int LastDigit = number % Convert.ToInt32(Math.Pow(10, 1)) / Convert.ToInt32(Math.Pow(10, 0));
+if (FirstDigit == LastDigit && SecondDigit == PenultimateDigit)
+{
+    Write("Это число палиндром");
+}
+else
+{
+    Write("Это число не палиндром");
+}
+ 
