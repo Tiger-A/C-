716e26156878fb60162859a3d7a023ce4f385be8
Alexei Efremov <td-interface@yandex.ru>
2022-08-20 12:39:15 +0400

    Заполняем массив случайными 0 и 1 и печатаем

---
+++ Practic/Lesson4/Example4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Заполняем массив случайными 0 и 1
+
+using static System.Console;
+Clear();
+
+int[] array = new int [10];
+FillArr(array);
+Write("[");
+PrintArr(array);
+Write("]");
+
+
+void FillArr (int[] arr)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+
+        arr[i] = new Random().Next(0, 2);
+    }
+
+}
+
+void PrintArr (int[] arr)
+{
+    for(int i=0; i <arr.Length; i++)
+    {
+        Write($"{arr[i]}");
+        if(i < arr.Length-1) Write(", ");
+    }
+
+
+
+
+}
