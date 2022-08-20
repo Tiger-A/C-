066c1d5137bd4d8fb3832e25529b615e14fbdc6a
Alexei Efremov <td-interface@yandex.ru>
2022-08-20 11:24:44 +0400

    Пример ис фунции сумма числа от 1 до N

---
+++ Practic/Lesson4/Example1/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+Write("ВВедите чило: ");
+int A=int.Parse(ReadLine());
+int sum=GetSum(A);
+WriteLine($"Сумма 1 - {A} = {sum}");
+
+
+
+
+int GetSum(int number)
+{
+    int result=0;
+    for(int i=1;i<=number;i++)
+        {
+            result+=i;
+        }
+    return result;
+}
