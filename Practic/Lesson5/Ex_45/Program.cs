ad8af2fd928289c456ad445f068e5d9825760147
Alexei Efremov <td-interface@yandex.ru>
2022-08-22 22:59:54 +0400

    Занятие 5

---
+++ Practic/Lesson5/Ex_45/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+/*Задача 44: Не используя рекурсию, выведите первые N 
+чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.*/
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число:");
+int number = Convert.ToInt32(ReadLine());
+int[] result = Fibonacci(number);
+WriteLine($"[{String.Join(",", result)}]");
+
+int[] Fibonacci (int n)
+{
+    int[] arr = new int[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i = 2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr;
+}
+
+
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+WriteLine($"{String.Join(" ", Fibb(N))}");
+
+int[] Fibb(int NN)
+{
+    int[] Fib = new int[NN];
+    //
+    Fib[0] = 0;
+    Fib[1] = 1;
+    for (int i = 2; i < NN; i++)
+    {
+        Fib[i] = Fib[i - 1] + Fib[i - 2];
+    }
+    return Fib;
+}
