541e008daf2bb0343c4a3a3abaa680b39646b41e
Alexei Efremov <td-interface@yandex.ru>
2022-08-22 23:13:02 +0400

    посчитать кол-во нечетнык элементы в массиве

--- Practic/Lesson5/Ex_45/Program.cs
+++ Practic/Lesson5/Ex_45/Program.cs
@@ -41,3 +41,14 @@ int[] Fibb(int NN)
     }
     return Fib;
 }
+
+
+//   посчитать нечетнык элементы в массиве 
+// using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите массив через пробел:");
+
+int count = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray().Where(a=>a%2!=0).Count();
+
+WriteLine(count);
