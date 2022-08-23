87259d300923b60146ed5ada3a498fba67ed6c73
Alexei Efremov <td-interface@yandex.ru>
2022-08-23 14:28:28 +0400

    Добавил распечатку строчого двумерный массив

--- Lections/Lection4/Program.cs
+++ Lections/Lection4/Program.cs
@@ -1,4 +1,40 @@
-﻿void PrintArray(int[,] matr)
+﻿//
+string[,] table = new string[2,5];
+// String.Empty
+// table [0,0] table [0,1] table [0,2] table [0,4]
+// table [1,0] table [1,1] table [1,2] table [1,4]
+table[1, 2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+ for (int columns = 0; columns < 5; columns++)
+ {
+ Console.WriteLine($"_{table[rows, columns]}_");
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Заполнение двумерной матрицы случайными числами и печать двуменрного массива.
+
+
+void PrintArray(int[,] matr)
 {
  for (int i = 0; i < matr.GetLength(0); i++)
  {
