d0e11ccdd60cc1abc49b5edfaaa6564e570c0370
Alexei Efremov <td-interface@yandex.ru>
2022-08-24 15:35:24 +0400

    Count sum digits in number

---
+++ Practic/Lesson4/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+using static System.Console;
+Clear();
+WriteLine("Введите число cумму цифр которого вы хотите узнать: ");
+int number = Convert.ToInt32(ReadLine());
+Console.WriteLine($"Cумма цифр в  введённом числе {SumDigitsInNumber(number)}");
+
+int SumDigitsInNumber(int number)
+{
+    int DigitsSum = 0;
+    for (int i = 0; i < number; i++)
+    {
+        DigitsSum += number % 10;
+        number = number / 10;
+    }
+    return DigitsSum;
+}
