d05410f0699f5e120d5388c787a9356c67fc42ed
Alexei Efremov <td-interface@yandex.ru>
2022-08-24 16:08:07 +0400

    Task29 программа, которая задаёт массив из 8 элементов и выводит их на экран ред2

--- Practic/Lesson4/Task29/Program.cs
+++ Practic/Lesson4/Task29/Program.cs
@@ -1,5 +1,33 @@
-﻿Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
 
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+using static System.Console;
+Clear();
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
 
-6, 1, 33 -> [6, 1, 33]
+void FillArray(int[] MyArray)
+{
+    int length = MyArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Write("Введите число массива  - ");
+        MyArray[index] = int.Parse(ReadLine()!);
+        index++;
+    }
+}
+
+void PrintArray(int[] AnyArray)
+{
+    int length = AnyArray.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Write($"{AnyArray[index]} ");
+        index++;
+    }
+}
+
