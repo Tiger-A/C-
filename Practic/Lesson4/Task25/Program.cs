49349e0f335b1359c37f7228412860df39c350cb
Alexei Efremov <td-interface@yandex.ru>
2022-08-24 14:50:47 +0400

    Одно число возводим в степень другого

---
+++ Practic/Lesson4/Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: 
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+using static System.Console;
+Clear();
+WriteLine("Возведем первое число в степнь второг числа. Введитете Первое число, нажмиьте Enter, введите второе число, нжмите Enter");
+int answer = RaiseNumberInPower(Convert.ToInt32(ReadLine()), Convert.ToInt32(ReadLine()));
+Write($"Ответ -  {answer}");
+
+int RaiseNumberInPower(int a, int b)
+{
+    int result = 1;    
+    for (int i = 0; i < b; i++) 
+    {
+        result *= a;
+    }
+    return result;
+}
