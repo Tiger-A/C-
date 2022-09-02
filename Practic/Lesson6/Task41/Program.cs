6d28c79f95dd82dd68c0b993cc684e70b1a45580
Alexei Efremov <td-interface@yandex.ru>
2022-09-02 13:51:03 +0400

    Do homework 41, 43

---
+++ Practic/Lesson6/Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 41: 
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+using System.Linq;
+Clear();
+
+
+Write("Введите через пробел несколько положительных и отрицательных целых чисел : ");
+WriteLine($"Введено положительных чисел - {FindCountPositiveElements(GetArrayFromString(ReadLine()!))}");
+
+
+int[] GetArrayFromString(string AnyStringArray)
+{
+    string[] MyStringArray = AnyStringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[MyStringArray.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(MyStringArray[i]);
+    }
+    WriteLine($"[{String.Join(",", result)}]");
+    return result;
+}
+
+
+int FindCountPositiveElements(int[] AnyArray)
+{
+    int count = 0;
+    foreach (int element in AnyArray)
+    {
+        count += element > 0 ? 1 : 0;
+    }
+    return count;
+}
+
+
+
---
+++ Practic/Lesson6/Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// Значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите коэффициенты b1, k1, b2, k2 чтобы найти точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2: ");
+WriteLine($"Точка пересечения по X: {IntersectionPoint(double.Parse(ReadLine()!), double.Parse(ReadLine()!), double.Parse(ReadLine()!), double.Parse(ReadLine()!))}");
+
+double IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double x = 0;
+   
+        x = (b2 - b1) / (k1 - k2);
+    
+    WriteLine($"Точка пересечения по Y: {(k1 * (b2 - b1)) / (k1 - k2) + b1}");
+    return x;
+}
