09d0017209b6dba347efa4f20ccbc2b212e2d5ce
Alexei Efremov <td-interface@yandex.ru>
2022-09-02 11:47:55 +0400

    Do homework 34, 36, 38

---
+++ Practic/Lesson5/Task36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: 
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using static System.Console;
+Clear();
+
+Write("Введите количество элементов в массиве: ");
+WriteLine($"Cумма элементов, стоящих на нечётных позициях в нашем массив: {SumOfNotEvenArrayElements(FillArrayWithRandomNum(int.Parse(ReadLine()!)))}");
+
+
+int SumOfNotEvenArrayElements(int[] array)
+{
+    int sum = 0;
+       for (int i = 1; i < array.Length; i+=2)
+    {
+        sum +=array[i];
+    }
+    return sum;
+}
+
+
+int[] FillArrayWithRandomNum(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+    WriteLine($"Массив заполненный случайными числами в диапазоне от -10 до 10 - [{String.Join(", ", array)}]");
+    return array;
+}
